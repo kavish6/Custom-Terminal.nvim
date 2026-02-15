004be24a646276503b342250dc6087c5752b8459
kavish6 <kavishdadhich@gmail.com>
2026-02-15 22:41:19 +0530

    Use logger util to notify on term opening and closing

--- plugin/custom-terminal.lua
+++ plugin/custom-terminal.lua
@@ -1,17 +1,18 @@
+local log = require("custom-terminal.util.log")
 if _G.CUSTOM_TERM_LOADED then
 	return
 end
 vim.api.nvim_create_autocmd("CmdlineEnter", {
 	pattern = { "*" },
 	callback = function()
-		vim.notify("Terminal opened")
+		log.notify("Terminal opened")
 	end,
 })
 
 vim.api.nvim_create_autocmd("CmdlineLeave", {
 	pattern = { "*" },
 	callback = function()
-		vim.notify("Terminal closed")
+		log.notify("Terminal closed")
 	end,
 })
 _G.CUSTOM_TERM_LOADED = true
